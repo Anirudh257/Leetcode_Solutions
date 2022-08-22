1dde035010a333017e94af89a0745bd908055dff
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-08-22 16:20:58 +0530

    Time: 724 ms (30.27%), Space: 49.8 MB (51.41%) - LeetHub

---
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> hashSet;
+        
+        for (int n : nums) {
+            hashSet.insert(n);
+        }
+        int longStreak = 0;
+        
+        for (int n : nums) {
+            if (!hashSet.count(n - 1)) {
+                int currStreak = 1;
+                
+                while(hashSet.count(n + 1)) {
+                    currStreak++;
+                    n++;
+                }
+                longStreak = max(longStreak, currStreak);
+            }
+        }
+        
+        return longStreak;
+    }
+};
