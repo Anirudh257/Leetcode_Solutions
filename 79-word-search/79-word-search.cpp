a89762c70c1986338ca05c528981b6b3b547212d
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-08-26 01:54:12 +0530

    Time: 1310 ms (8.50%), Space: 8 MB (76.85%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -4,16 +4,18 @@ public:
         int m = board.size(), n = board[0].size();
         int wordLen = word.length();
         
+        // Time complexity: O(m*n*dfs(len(word))), Space complexity: O(m*n)
+        
         vector<vector<bool>> visited(m, vector<bool>(n, false));
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && word[0] == board[i][j]) {
-                    bool ans = checkWord(board, visited, word, i, j, m, n, 0);
+                if (word[0] == board[i][j]) {
+                    bool ans = checkWord(board, word, i, j, m, n, 0);
                     if (ans) {
                         return true;
                     }
-                    visited[i][j] = false;
+                    // visited[i][j] = false;
                 }
             }
         }
@@ -21,48 +23,46 @@ public:
         return false;
     }
 private:
-    bool checkWord(vector<vector<char>>& board, vector<vector<bool>>& visited, string word, int i, int j, int m, int n, int ind) {
+    bool checkWord(vector<vector<char>>& board, string word, int i, int j, int m, int n, int ind) {
         if (ind  == word.size() - 1) {
             if (word[ind] == board[i][j]) {
                 return true;
             }
         }
         
-        if (visited[i][j]) {
-            return false;
-        }
-        
-        visited[i][j] = true;
-        
         if (word[ind] != board[i][j]) {
             return false;
         }
+        
+        char reqCh = board[i][j];
+        board[i][j] = '*';        
         
         bool leftAns = false;
         bool rightAns = false;
         bool upAns = false;
         bool downAns = false;
         
-        if (valid(i - 1, j, m, n) && !visited[i - 1][j]) {
-            upAns = checkWord(board, visited, word, i - 1, j, m, n, ind + 1);
-            visited[i - 1][j] = false;
+        if (valid(i - 1, j, m, n)) {
+            upAns = checkWord(board, word, i - 1, j, m, n, ind + 1);
+            // visited[i - 1][j] = false;
         }
         
-        if (valid(i + 1, j, m, n) && !visited[i + 1][j]) {
-            downAns = checkWord(board, visited, word, i + 1, j, m, n, ind + 1);
-            visited[i + 1][j] = false;
+        if (valid(i + 1, j, m, n)) {
+            downAns = checkWord(board, word, i + 1, j, m, n, ind + 1);
+            // visited[i + 1][j] = false;
         }
 
-        if (valid(i, j - 1, m, n) && !visited[i][j - 1]) {
-            leftAns = checkWord(board, visited, word, i, j - 1, m, n, ind + 1);
-            visited[i][j - 1] = false;
+        if (valid(i, j - 1, m, n)) {
+            leftAns = checkWord(board, word, i, j - 1, m, n, ind + 1);
+            // visited[i][j - 1] = false;
         }
         
-        if (valid(i, j + 1, m, n) && !visited[i][j + 1]) {
-            rightAns = checkWord(board, visited, word, i, j + 1, m, n, ind + 1);
-            visited[i][j + 1] = false;
+        if (valid(i, j + 1, m, n)) {
+            rightAns = checkWord(board, word, i, j + 1, m, n, ind + 1);
+            // visited[i][j + 1] = false;
         }
         
+        board[i][j] = reqCh;
         return leftAns || rightAns || upAns || downAns;
     }
 private:
