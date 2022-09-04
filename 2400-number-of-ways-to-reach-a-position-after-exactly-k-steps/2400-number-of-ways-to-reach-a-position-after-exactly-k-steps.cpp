fa4c83d7df285422c4ecb402076fca3af60d56a5
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-09-04 12:17:06 +0530

    Time: 508 ms (50.00%), Space: 41.2 MB (50.00%) - LeetHub

---
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int numberOfWays(int startPos, int endPos, int k) {
+        for (int i = 0; i < 3001; i++) {
+            for (int j = 0; j < 1001; j++) {
+                dp[i][j] = -1;
+            }
+        }       
+        
+        return countWays(startPos, endPos, k);
+        
+    }
+private:
+    long long int dp[4001][1001];
+    long long int mod = 1e9 + 7;
+    
+    long long countWays(int currPos, int endPos, int k) {
+        if (k == 0 && currPos == endPos) {
+            return 1;
+        }
+        
+        if (k == 0) {
+            return 0;
+        }
+        
+        if (dp[currPos + 1000][k] != -1) {
+            return dp[currPos + 1000][k];
+        }
+        
+        long long numWays = countWays(currPos - 1, endPos, k - 1);
+        numWays += countWays(currPos + 1, endPos, k - 1);
+        
+        return dp[currPos + 1000][k] = numWays%mod;
+        
+    }
+    
+};
