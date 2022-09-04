9d85362608dcf598e600509787e4c867dca71cc5
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-09-04 15:22:50 +0530

    Time: 7 ms (12.82%), Space: 6 MB (28.24%) - LeetHub

---
+++ 754-reach-a-number/754-reach-a-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int reachNumber(int target) {
+        int numSteps = 0, currSum = 0;
+        
+        if (target == 0) {
+            return 0;
+        }
+        
+        target = abs(target);
+        
+        while(currSum < target) {
+            currSum += numSteps;
+            numSteps++;
+        }
+        
+        if (currSum == target) {
+            return numSteps - 1;
+        }
+        
+        while((currSum - target)%2 == 1) {
+            currSum += numSteps;
+            numSteps++;
+            
+        }
+        
+        return numSteps - 1;
+    }
+};
