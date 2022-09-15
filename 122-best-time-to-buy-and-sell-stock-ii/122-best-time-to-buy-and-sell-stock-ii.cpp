7d199c44867c7bcf30156f3e94af613fdc221466
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-09-15 06:17:26 -0700

    Time: 15 ms (41.15%), Space: 17.8 MB (5.96%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int len = prices.size();
+        vector<vector<int>> dp(len, vector<int>(2, -1));
+        
+        return compProf(prices, 0, len, dp,  true);   
+    }
+private:
+    // Time complexity: O(2^n), Space complexity: O(n)
+    int compProf(vector<int>& prices, int ind, int len, vector<vector<int>>& dp, bool buy) {
+        if (ind == len) {
+            return 0;
+        }   
+        if (dp[ind][buy] != -1) {
+            return dp[ind][buy];
+        }
+        int profit = 0;
+        
+        if (buy) {
+            dp[ind][buy] = max(-prices[ind] + compProf(prices, ind + 1, len, dp, false), compProf(prices, ind + 1, len, dp, true));
+        }
+        else {
+            dp[ind][buy] = max(prices[ind] + compProf(prices, ind + 1, len, dp, true), compProf(prices, ind + 1, len, dp, false));    
+        }
+        
+        return dp[ind][buy];
+    }
+};
