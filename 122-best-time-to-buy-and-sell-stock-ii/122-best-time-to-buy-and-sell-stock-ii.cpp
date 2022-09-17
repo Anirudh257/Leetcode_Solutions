6abd039b7fadf479e64913819888abcf8048b301
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-09-17 07:04:35 -0700

    Time: 15 ms (41.65%), Space: 13 MB (89.99%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        int prof = 0;
+        int minVal = prices[0], maxVal = prices[0];
         int len = prices.size();
-        // Time complexity: O(nx2), Space complexity: O(nx2) + Auxiliary Stack Space O(N)
-        vector<vector<int>> dp(len, vector<int>(2, -1));
         
-        return compProf(prices, 0, len, dp,  true);   
-    }
-private:
-    // Time complexity: O(2^n), Space complexity: O(n)
-    int compProf(vector<int>& prices, int ind, int len, vector<vector<int>>& dp, bool buy) {
-        if (ind == len) {
-            return 0;
-        }   
-        if (dp[ind][buy] != -1) {
-            return dp[ind][buy];
-        }
-        int profit = 0;
         
-        if (buy) {
-            dp[ind][buy] = max(-prices[ind] + compProf(prices, ind + 1, len, dp, false), compProf(prices, ind + 1, len, dp, true));
-        }
-        else {
-            dp[ind][buy] = max(prices[ind] + compProf(prices, ind + 1, len, dp, true), compProf(prices, ind + 1, len, dp, false));    
+        for (int i = 1; i < len; i++) {
+            if (prices[i] < minVal) {
+                prof += maxVal - minVal;
+                
+                minVal = maxVal = prices[i];
+            }
+            else {
+                if (prices[i] > maxVal) {
+                    maxVal = prices[i];
+                }
+                else {
+                    prof += maxVal - minVal;
+                    maxVal = minVal = prices[i];
+                }
+            }
         }
         
-        return dp[ind][buy];
+        prof += maxVal - minVal;
+        
+        return prof;
     }
 };
