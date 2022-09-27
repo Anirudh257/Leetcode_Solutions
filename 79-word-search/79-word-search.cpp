61fa939c8cf5de32a9c3d9797570fa370b00ce74
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-09-27 07:42:47 -0700

    Update 79-word-search.cpp

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -2,75 +2,42 @@ class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
         int m = board.size(), n = board[0].size();
-        int wordLen = word.length();
         
-        // Time complexity: O(m*n*dfs(len(word))), Space complexity: O(m*n)
-        // 4 as we have 4 possible directions to go for each character
-
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        // Time complexity: O(mn4^(word.size())), Space complexity: O(mn)
         
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+            for(int j = 0; j < n; j++) {
                 if (word[0] == board[i][j]) {
-                    bool ans = checkWord(board, word, i, j, m, n, 0);
-                    if (ans) {
+                    if (searchWord(board, word, i, j, 0, m, n)) {
                         return true;
                     }
-                    // visited[i][j] = false;
                 }
             }
         }
-        
         return false;
+        
     }
 private:
-    bool checkWord(vector<vector<char>>& board, string word, int i, int j, int m, int n, int ind) {
-        if (ind  == word.size() - 1) {
-            if (word[ind] == board[i][j]) {
-                return true;
-            }
-        }
-        
-        if (word[ind] != board[i][j]) {
-            return false;
-        }
-        
-        char reqCh = board[i][j];
-        board[i][j] = '*';        
-        
-        bool leftAns = false;
-        bool rightAns = false;
-        bool upAns = false;
-        bool downAns = false;
-        
-        if (valid(i - 1, j, m, n)) {
-            upAns = checkWord(board, word, i - 1, j, m, n, ind + 1);
-            // visited[i - 1][j] = false;
-        }
-        
-        if (valid(i + 1, j, m, n)) {
-            downAns = checkWord(board, word, i + 1, j, m, n, ind + 1);
-            // visited[i + 1][j] = false;
-        }
-
-        if (valid(i, j - 1, m, n)) {
-            leftAns = checkWord(board, word, i, j - 1, m, n, ind + 1);
-            // visited[i][j - 1] = false;
-        }
-        
-        if (valid(i, j + 1, m, n)) {
-            rightAns = checkWord(board, word, i, j + 1, m, n, ind + 1);
-            // visited[i][j + 1] = false;
-        }
-        
-        board[i][j] = reqCh;
-        return leftAns || rightAns || upAns || downAns;
-    }
-private:
-    bool valid(int i, int j, int m, int n) {
-        if (i >= 0 && i < m && j >= 0 && j < n) {
+    bool searchWord(vector<vector<char>>& board, string word, int i, int j, int ind, int m, int n) { 
+        if (ind == word.size()) {
             return true;
         }
-        return false;
+        
+        if (i < 0 || i >= m || j < 0 || j >= n || word[ind] != board[i][j]) {
+            return false;
+        }
+
+        char ch = board[i][j];
+        board[i][j] = '#';
+        
+        bool leftAns = searchWord(board, word, i, j - 1, ind + 1, m, n);
+        bool rightAns = searchWord(board, word, i, j + 1, ind + 1, m, n);
+        bool upAns = searchWord(board, word, i - 1, j, ind + 1, m, n);
+        bool downAns = searchWord(board, word, i + 1, j, ind + 1, m, n);
+        
+        board[i][j] = ch;
+        
+        return leftAns || rightAns || upAns || downAns;
+        
     }
 };
