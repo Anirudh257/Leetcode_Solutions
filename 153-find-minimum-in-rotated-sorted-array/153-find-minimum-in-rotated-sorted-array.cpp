c3ddf511619114409cb6e6a29832e0a985f2199f
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-09-29 16:04:47 -0700

    Time: 10 ms (24.13%), Space: 10.2 MB (22.12%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -2,41 +2,36 @@ class Solution {
 public:
     int findMin(vector<int>& nums) {
         int size = nums.size();
-        // Time complexity: O(log n), Space complexity: O(1)
+        
         if (nums[0] < nums[size - 1]) {
             return nums[0];
         }
         
-        return calcMin(nums, 0, size - 1);
+        return compMinElem(nums, 0, size - 1);
     }
 private:
-    int calcMin(vector<int>& nums, int l, int r) {        
-        while(l < r) {
-            int mid = (l + r)/2;
-            
-            if (mid - 1 >= 0 && nums[mid] < nums[mid - 1] && mid + 1 < nums.size() && nums[mid] < nums[mid + 1]) {
-                return nums[mid];
-            }
+    int compMinElem(vector<int>& nums, int l, int r) {
+        int res = INT_MAX;
+        while(l <= r) {
             
             if (nums[l] < nums[r]) {
-                if (nums[mid] < nums[r]) {
-                    r = mid - 1;
-                }
-                else {
-                    l = mid + 1;
-                }
-            }
-            else {
-                if (nums[mid] < nums[l]) {
-                    r = mid - 1;
-                }
-                else {
-                    l = mid + 1;
-                }
+                res = min(nums[l], res);
             }
             
+            int mid = (l + r)/2;
+            
+            // cout << mid << endl;
+            res = min(res, nums[mid]);
+            
+            // cout << res << endl;
+            
+            if (nums[mid] >= nums[l]) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
         }
-        return nums[l];
-        
+        return res;
     }
 };
