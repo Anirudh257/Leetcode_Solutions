3322bb3dcebfade801ff88024dfffaef89ec2206
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-10-08 16:36:02 -0700

    Time: 417 ms (5.97%), Space: 18.4 MB (69.47%) - LeetHub

--- 498-diagonal-traverse/498-diagonal-traverse.cpp
+++ 498-diagonal-traverse/498-diagonal-traverse.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
-            if(matrix.empty()) return {};
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        // Time complexity: O(m*n), Space complexity: O(m*n)
+         int m = mat.size(), n = mat[0].size();
+         vector<int> finOrd;
 
-            const int N = matrix.size();
-            const int M = matrix[0].size();
-
-            vector<int> res;
-            for(int s = 0; s <= N + M - 2; ++s)
-            {
-                // for all i + j = s
-                for(int x = 0; x <= s; ++x) 
-                {
+         for (int d = 0; d <= m + n - 2; d++) {
+              for (int x = 0; x <= d; x++) {
                     int i = x;
-                    int j = s - i;
-                    if(s % 2 == 0) swap(i, j);
+                    int j = d - i;
+                    
+                    if (d % 2 == 0) {
+                        swap(i, j);
+                    }
 
-                    if(i >= N || j >= M) continue;
+                    if (i >= m || j >= n) {
+                        continue;
+                    }                    
 
-                    res.push_back(matrix[i][j]);
-                }
-            }
-
-            return res;
-        }
+                    finOrd.push_back(mat[i][j]);
+              }
+         }
+         return finOrd;
+    }
 };
