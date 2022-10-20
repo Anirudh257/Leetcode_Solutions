83245ef024a4c072dfdef8a16dcc1b27c3baf9ec
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-10-20 12:17:13 -0700

    Time: 7 ms (21.98%), Space: 5.8 MB (78.91%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // Time complexity: O(k), k = number of bits, Space complexity: O(1)
     int hammingWeight(uint32_t n) {
         int count = 0;
         while(n > 0) {
