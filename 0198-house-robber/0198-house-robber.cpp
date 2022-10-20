f69e8b917cc95d5cfc10aa08e8a67754484afe94
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-10-20 13:01:02 -0700

    Time: 3 ms (56.54%), Space: 7.9 MB (25.78%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> dp(size, -1);
+        
+        return compMaxAmt(nums, 0, size, 0, dp);
+    }
+private:
+    int compMaxAmt(vector<int>& nums, int ind, int size, int amt, vector<int>& dp) {
+        if (ind >= size) {
+            return 0;
+        }
+        
+        if (dp[ind] != -1) {
+            return dp[ind];
+        }
+        
+        int take = 0;
+        int notTake = 0;
+        
+        take = nums[ind] + compMaxAmt(nums, ind + 2, size, amt, dp);
+        notTake = compMaxAmt(nums, ind + 1, size, amt, dp);
+        
+        return dp[ind] = max(take, notTake);
+    }
+};
