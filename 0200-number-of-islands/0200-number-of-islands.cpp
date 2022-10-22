9448ea629deb2fc2ba7d140352bb62971a4c989c
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-10-22 06:38:15 -0700

    Time: 94 ms (19.69%), Space: 12.7 MB (36.68%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<bool>> visit(m, vector<bool>(n, false));
+        int numIsl = 0;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visit[i][j] && grid[i][j] == '1') {
+                    // cout << i << " " << j << endl;
+                    compIslands(grid, i, j, visit, m, n);
+                    numIsl++;
+                }
+            } 
+        }
+        return numIsl;
+    }
+private:
+    void compIslands(vector<vector<char>>& grid, int i, int j, vector<vector<bool>>& visit, int m, int n) {
+        if (i < 0 || i >= m || j < 0 || j >= n || visit[i][j] || grid[i][j] != '1') {
+            return;
+        }
+        visit[i][j] = true;
+        
+        compIslands(grid, i, j - 1, visit, m, n);
+        compIslands(grid, i, j + 1, visit, m, n);
+        compIslands(grid, i - 1, j, visit, m, n);
+        compIslands(grid, i + 1, j, visit, m, n);
+        
+    }
+};
