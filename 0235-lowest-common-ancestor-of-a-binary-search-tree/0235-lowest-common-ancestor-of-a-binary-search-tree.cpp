2951ac4300dccee48a762edc50418eb215847c5e
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-11-09 07:31:04 -0800

    Time: 73 ms (24.01%), Space: 23.4 MB (19.08%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode*> pathP, pathQ;
+        compPath(root, p, pathP);
+        compPath(root, q, pathQ);
+        
+//         for (auto &x : pathP) {
+//             cout << x->val << endl;
+//         }
+        
+//         for (auto &x : pathQ) {
+//             cout << x->val << endl;
+//         }        
+        
+        TreeNode* lca = root;
+        
+        
+        for(int i = 0; i < min(pathP.size(), pathQ.size()); i++) {
+            int pVal = pathP[i]->val;
+            int qVal = pathQ[i]->val;
+            
+            
+            
+            if (pVal == qVal) {
+                lca = pathP[i];
+            }
+            else {
+                return lca;
+            }
+            
+        }
+        
+        
+        
+        return lca;
+    }
+private:
+    void compPath(TreeNode* curr, TreeNode* node, vector<TreeNode*>& path) {
+        if (curr == node) {
+            path.push_back(curr);
+            
+            return;
+        }
+        
+        path.push_back(curr);
+        
+        if (node->val < curr->val) {
+            // path.push_back(node);
+            compPath(curr->left, node, path);
+        }
+        else {
+            // path.push_back(node);
+            compPath(curr->right, node, path);
+        }
+        return;
+    }
+};
