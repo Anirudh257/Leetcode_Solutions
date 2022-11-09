aa5b66f88df73f17d9980f999b1603e1a8edcf72
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-11-09 07:41:44 -0800

    Create optimal_iterative_approach.cpp

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/optimal_iterative_approach.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    // Time complexity: O(N), Space complexity: O(1)
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* curr = root;
+        
+        while(true) {
+            if ((curr->val > p->val) && (curr->val > q->val)) {
+                curr = curr->left;
+            }
+            else if ((curr->val < p->val) && (curr->val < q->val)) {
+                curr = curr->right;
+            }
+            else {
+                break;
+            }
+        }
+        return curr;
+    }
+};
