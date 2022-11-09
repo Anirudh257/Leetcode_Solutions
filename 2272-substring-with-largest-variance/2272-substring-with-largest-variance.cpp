38d4a1781e948250e1cda040cac7120e053a9a93
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-11-09 06:25:33 -0800

    Time: 137 ms (78.32%), Space: 8.1 MB (25.60%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
@@ -5,19 +5,21 @@ public:
         unordered_set<char> unique(begin(s), end(s));
         for (char a : unique)
             for (char b : unique) {
-                int var = 0, has_b = 0, first_b = 0;
-                for (auto ch : s) {
-                    var += ch == a;
-                    if (ch == b) {
-                        has_b = true;
-                        if (first_b && var >= 0)
-                            first_b = false;
-                        else if (--var < 0) {
-                            first_b = true;
-                            var = -1;
+                if (a != b) {
+                    int var = 0, has_b = 0, first_b = 0;
+                    for (auto ch : s) {
+                        var += ch == a;
+                        if (ch == b) {
+                            has_b = true;
+                            if (first_b && var >= 0)
+                                first_b = false;
+                            else if (--var < 0) {
+                                first_b = true;
+                                var = -1;
+                            }
                         }
-                    }
-                    res = max(res, has_b ? var : 0);
+                        res = max(res, has_b ? var : 0);
+                    }                    
                 }
             }
         return res;
