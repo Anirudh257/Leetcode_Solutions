048690d8c81a63ba62c5eb0b73c6b5ad6eda754c
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-11-10 09:39:00 -0800

    Time: 49 ms (19.78%), Space: 11.8 MB (43.03%) - LeetHub

---
+++ 0252-meeting-rooms/0252-meeting-rooms.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        int size = intervals.size();
+        
+        if (size <= 1) {
+            return true;
+        }
+        
+        sort(intervals.begin(), intervals.end());
+        
+        int maxSt = intervals[0][0];
+        int maxEnd = intervals[0][1];
+        
+        
+        for (int i = 1; i < size; i++) {
+            int currSt = intervals[i][0];
+            int currEnd = intervals[i][1];   
+            
+            if (currSt < maxEnd) {
+                return false;
+            }
+            maxSt = min(maxSt, currSt);
+            maxEnd = max(maxEnd, currEnd);
+            
+        }
+        
+        return true;
+    }
+};
