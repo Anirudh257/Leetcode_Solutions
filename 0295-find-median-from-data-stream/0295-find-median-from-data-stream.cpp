db7938ae1324fc1bc88205f9655e5387129ceef5
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-11-12 12:34:31 -0800

    Time: 925 ms (6.27%), Space: 117.1 MB (19.58%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,25 +1,43 @@
 class MedianFinder {
-    priority_queue<int> lo;                              // max heap
-    priority_queue<int, vector<int>, greater<int>> hi;   // min heap
-
 public:
-    // Adds a number into the data structure.
-    void addNum(int num)
-    {
-        lo.push(num);                                    // Add to max heap
-
-        hi.push(lo.top());                               // balancing step
-        lo.pop();
-
-        if (lo.size() < hi.size()) {                     // maintain size property
-            lo.push(hi.top());
-            hi.pop();
+    MedianFinder() {
+           
+    }
+    
+    void addNum(int num) {
+        maxHeap.push(num);
+        
+        minHeap.push(maxHeap.top()); // Add element to lower half
+        
+        maxHeap.pop(); 
+        
+        // Balance tree ensuring that maxHeap.size == minHeap.size() always
+        
+        if (maxHeap.size() < minHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
         }
+        
     }
-
-    // Returns the median of current data stream
-    double findMedian()
-    {
-        return lo.size() > hi.size() ? lo.top() : ((double) lo.top() + hi.top()) * 0.5;
+    
+    double findMedian() {
+        if (maxHeap.size() > minHeap.size()) {
+            med = maxHeap.top();
+        }
+        else {
+            med = (double)(maxHeap.top() + minHeap.top())/2;
+        }
+        return med;
     }
+private:
+    priority_queue<int> maxHeap; // First half sorted in the ascending order and top of 1st half
+    priority_queue<int, vector<int>, greater<int>> minHeap; // 2nd half in ascending order and get top of 2nd half.
+    double med;
 };
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
