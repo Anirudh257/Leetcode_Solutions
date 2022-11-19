c541ec5a8d2ddfc3ac1f69aa53e612bf9c0c81b5
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-11-19 15:53:31 -0800

    Time: 4 ms (62.52%), Space: 7 MB (95.60%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    // Time complexity: O(2^n*n), Space complexity: O(1)
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> subs;
+        int size = nums.size();
+        int numSubs = (1 << size); // 2^size
+        
+        for (int num = 0; num < numSubs; num++) {
+            vector<int> sub;
+            for (int j = 0; j < size; j++) {
+                // pick based on index
+                if (num & (1 << j)) {
+                    sub.push_back(nums[j]);
+                }
+            }
+            subs.push_back(sub);
+        }
+        
+        
+        return subs;
+    }
+};
