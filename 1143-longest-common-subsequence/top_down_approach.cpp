e0f322239d1db0db84d48ab1529f894bee6dd5ca
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-11-19 17:18:43 -0800

    Update top_down_approach.cpp

--- 1143-longest-common-subsequence/top_down_approach.cpp
+++ 1143-longest-common-subsequence/top_down_approach.cpp
@@ -26,21 +26,4 @@ public:
         
         return dp[m][n];
     } 
-private:
-    int compLCS(string& text1, string& text2, int ind1, int ind2, vector<vector<int>>& dp) {
-        if (ind1 < 0 || ind2 < 0) {
-            return 0;
-        }
-        if (dp[ind1][ind2] != -1) {
-            return dp[ind1][ind2];
-        }
-        if (text1[ind1] == text2[ind2]) {
-            dp[ind1][ind2] =  1 + compLCS(text1, text2, ind1 - 1, ind2 - 1, dp);
-        }
-        else {
-            dp[ind1][ind2] = max(compLCS(text1, text2, ind1 - 1, ind2, dp), compLCS(text1, text2, ind1, ind2 - 1, dp));        
-        }
-        
-        return dp[ind1][ind2];
-    }
 };
