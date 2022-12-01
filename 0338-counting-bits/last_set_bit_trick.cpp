73a6080fccf804eec1f0140be0ffb5efbee5af63
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-12-01 11:02:32 -0800

    Update last_set_bit_trick.cpp

--- 0338-counting-bits/last_set_bit_trick.cpp
+++ 0338-counting-bits/last_set_bit_trick.cpp
@@ -6,7 +6,7 @@ public:
             return {0};
         }
         vector<int> bits(n + 1, 0);
-        
+        // n & (n - 1) clears the last set bit of n
         for (int i = 1; i <= n; i++) {
             bits[i] = bits[i & (i - 1)] + 1;
         }
