b49014619e72c987a3c6e59a06272f729df2b85f
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-12-01 10:57:55 -0800

    Time: 11 ms (47.40%), Space: 7.9 MB (49.59%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -8,18 +8,15 @@ public:
         
         if (n == 1) {
             return {0, 1};
-        }
+        }        
         
-        
-        vector<int> bits(n + 1, -1);
+        vector<int> bits(n + 1, 0);
         bits[0] = 0;
-        bits[1] = 1;
-        bits[2] = 1;
         
-        for(int i = 3; i<= n; i++) {
-            if (bits[i] == -1) {
-                bits[i] = bits[i>>1] + bits[i%2];
-            }
+        for(int i = 1; i<= n; i++) {
+            // if (bits[i] == -1) {
+            bits[i] = bits[i>>1] + i % 2;
+            // }
         }
         
         return bits;
