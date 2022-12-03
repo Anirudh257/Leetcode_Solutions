73a26489b4e50b1485870b34970aee1d67973e0a
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-12-03 00:35:19 -0800

    Time: 0 ms (100.00%), Space: 6.1 MB (25.75%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        if (b == 0) {
+            return a;
+        }
+        
+        double powSum = pow(10, a)*pow(10, b);
+        double sum = log10(powSum);
+        
+        return (int)sum;   
+    }
+};
