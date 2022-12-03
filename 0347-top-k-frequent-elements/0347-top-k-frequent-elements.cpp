020948726a28283de6b3705321f490f3d5bee83c
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2022-12-02 20:05:31 -0800

    Time: 19 ms (86.92%), Space: 14 MB (17.46%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    bool static cmp(int a, int b) {
+        if (a > b) {
+            return false;
+        }
+        return true;
+    }
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // priority_queue<int, vector<int>, cmp> pq;
+        unordered_map<int, vector<int>> freqNum;
+        unordered_map<int, int> numFreq;
+        // unordered_map<int, int> indFreq;
+        vector<int> res;
+        int size = nums.size();
+        for (int i = 0; i < size; i++) {
+            int num = nums[i];
+            numFreq[num]++;
+        }
+        
+        int maxFreq = INT_MIN;
+        
+        for (auto &x : numFreq) {
+            int freq = x.second;
+            int num = x.first;
+            freqNum[freq].push_back(num);
+            maxFreq = max(maxFreq, freq);
+        }        
+        
+        while(res.size() <= k) {
+            if (freqNum.find(maxFreq) != freqNum.end()) {
+                vector<int> elems = freqNum[maxFreq];
+                
+                if (elems.size() == k) {
+                    return elems;
+                }
+                else {
+                    for (int elem : elems) {
+                        res.push_back(elem);
+                        if (res.size() == k) {
+                            return res;
+                        }
+                    }
+                }
+                
+                
+            }
+            maxFreq--;
+        }
+        
+        
+        
+        return res;
+    }   
+private:
+};
