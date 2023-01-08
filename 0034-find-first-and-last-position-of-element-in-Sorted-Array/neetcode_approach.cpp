e9d380aa8e59812533f7d16e64bd0d2ffba12a0c
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2023-01-08 12:37:58 -0800

    Update neetcode_approach.cpp

--- 0034-find-first-and-last-position-of-element-in-Sorted-Array/neetcode_approach.cpp
+++ 0034-find-first-and-last-position-of-element-in-Sorted-Array/neetcode_approach.cpp
@@ -9,19 +9,26 @@ public:
             return {-1, -1};
         }
 
-        reqRange[0] = searchLeftMost(nums, target, 0, size - 1);
-        reqRange[1] = searchRightMost(nums, target, 0, size - 1);
+        reqRange[0] = binSearch(nums, target, 0, size - 1, true);
+        reqRange[1] = binSearch(nums, target, 0, size - 1, false);
 
         return reqRange;
     }
 private:
-    int searchLeftMost(vector<int>& nums, int target, int left, int right) {
-        int leftInd = -1;
+    int binSearch(vector<int>& nums, int target, int left, int right, bool leftBias) {
+        int reqInd = -1;
         while(left <= right) {
             int mid = (left + right) >> 1;
             if (nums[mid] == target) {
-                leftInd = mid;
-                right = mid - 1;
+                if (leftBias) {
+                    // Keep going left
+                    right = mid - 1;
+                }
+                else {
+                    // Keep going right
+                    left = mid + 1;
+                }
+                reqInd = mid;
             }
             else if (nums[mid] > target) {
                 right = mid - 1;
@@ -30,24 +37,6 @@ private:
                 left = mid + 1;
             }
         }
-        return leftInd;
+        return reqInd;
     }
-    int searchRightMost(vector<int>& nums, int target, int left, int right) {
-        int rightInd = -1;
-        while(left <= right) {
-            int mid = (left + right) >> 1;
-            if (nums[mid] == target) {
-                rightInd = mid;
-                left = mid + 1;
-            }
-            else if (nums[mid] > target) {
-                right = mid - 1;
-            }
-            else {
-                left = mid + 1;
-            }
-        }
-        return rightInd;
-    }
-
 };
