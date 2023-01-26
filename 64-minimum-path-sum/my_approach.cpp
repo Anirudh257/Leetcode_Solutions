85687f0e70de401fd354aefd18e1ff0a2f5e1cca
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2023-01-26 15:01:54 -0800

    Create my_approach.cpp

---
+++ 64-minimum-path-sum/my_approach.cpp
@@ -0,0 +1,26 @@
+// Time complexity: O(MN), Space complexity: O(MN) + Auxiliary Space complexity: O(N)
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return compMinPath(grid, 0, 0, m, n, 0, dp);
+    }
+private:
+    int compMinPath(vector<vector<int>>& grid, int i, int j, int m, int n, int cost, vector<vector<int>>& dp) {
+        if (i < 0 || j < 0 || i >= m || j >= n) {
+            return 1e9;
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        if (i == m - 1 && j == n - 1) {
+            return grid[i][j];
+        }
+
+        return dp[i][j] = grid[i][j] + min(compMinPath(grid, i + 1, j, m, n, cost, dp), compMinPath(grid, i, j + 1, m, n, cost,dp));
+
+    }
+};
