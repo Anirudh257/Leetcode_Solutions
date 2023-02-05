75b946d9f4d1e57b44da9f62273c07e19a3c3c94
Anirudh257 <thatipellianirudh@gmail.com>
2023-02-04 16:47:30 -0800

    Added code for 701 Leetcode question

---
+++ 0701-insert-into-a-binary-search-tree/my_approach.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (!root) {
+            TreeNode* newRoot = new TreeNode(val);
+            return newRoot;
+        }
+
+        perfInsertion(root, val);
+        return root;
+    }
+private:
+    void perfInsertion(TreeNode* node, int val) {
+        if (!node) {
+            return;
+        }
+        if (node && !node->right && val > node->val) {
+            TreeNode* newNode = new TreeNode(val);
+            node->right = newNode;
+            return;
+        }
+
+        if (node && !node->left && val < node->val) {
+            TreeNode* newNode = new TreeNode(val);
+            node->left = newNode;
+            return;
+
+        }
+
+        if (node && val > node->val) {
+            perfInsertion(node->right, val);
+        }
+        else {
+            perfInsertion(node->left, val);
+        }
+    }
+};
