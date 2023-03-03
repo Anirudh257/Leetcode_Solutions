f93e48f13b80f3e01ddeddab592c8538046122e5
Anirudh257 <thatipellianirudh@gmail.com>
2023-03-03 08:28:47 -0800

    Added solution for 0/1-Knapsack

--- 1143-longest-common-subsequence/top_down_approach.cpp
+++ 1143-longest-common-subsequence/top_down_approach.cpp
@@ -3,8 +3,9 @@ public:
     // Time complexity: O(mn), Space complexity: O(mn) 
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.size(), n = text2.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
 
+        // Shifted index. dp[0] == text1[-1]
         for (int i = 0; i <= m; i++) {
             dp[i][0] = 0;
         }
@@ -13,6 +14,7 @@ public:
             dp[0][j] = 0;
         }
         
+        // Perform shifting of indices to check for base case: (ind1 < 0 || ind2 < 0)
         for (int ind1 = 1; ind1 <= m; ind1++) {
             for (int ind2 = 1; ind2 <= n; ind2++) {
                 if (text1[ind1 - 1] == text2[ind2 - 1]) {
