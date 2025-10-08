199216d7a05007aa4690baf0aea2e4dd89ce8200
Anirudh257 <thatipellianirudh@gmail.com>
2025-10-08 16:33:33 -0400

    Add a Python solution to the longest common subsequence problem

---
+++ 1143-longest-common-subsequence/Python_Solns/top_down_approach.py
@@ -0,0 +1,31 @@
+# Time complexity: O(N^2), Space complexity: O(N)
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        size1 = len(text1)
+        size2 = len(text2)
+
+        self.dp = [[-1 for _ in range(size2)] for _ in range(size1)]
+
+        return self.compLCSLen(text1, text2, size1 - 1, size2 - 1)
+
+    def compLCSLen(self, text1: str, text2: str, ind1 : int, ind2: int) -> int:
+
+        if ind1 < 0 or ind2 < 0:
+            return 0
+
+        if self.dp[ind1][ind2] != -1:
+            return self.dp[ind1][ind2]
+
+        ch1 = text1[ind1]
+        ch2 = text2[ind2]
+        
+        curr_lcs_len = float('-inf')
+        if ch1 == ch2:
+            curr_lcs_len = 1 + self.compLCSLen(text1, text2, ind1 - 1, ind2 - 1)
+        else:
+            curr_lcs_len = max(self.compLCSLen(text1, text2, ind1 - 1, ind2), self.compLCSLen(text1, text2, ind1, ind2 - 1))
+
+        self.dp[ind1][ind2] = curr_lcs_len
+
+        return self.dp[ind1][ind2]
