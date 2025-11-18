d4191dd6edabfd8232fbf8f53fb6ae70f1f99386
Anirudh257 <thatipellianirudh@gmail.com>
2025-11-18 00:59:27 -0500

    Modify longest-common-subsequence code.

--- 1143-longest-common-subsequence/Python_Solns/bottom_up_approach.py
+++ 1143-longest-common-subsequence/Python_Solns/bottom_up_approach.py
@@ -5,7 +5,7 @@ class Solution:
         size1 = len(text1)
         size2 = len(text2)
 
-        self.dp = [[0 for _ in range(size2 + 1)] for _ in range(size1 + 1)]
+        self.dp = [[-1 for _ in range(size2 + 1)] for _ in range(size1 + 1)]
 
         for ind1 in range(size1 + 1):
             self.dp[ind1][0] = 0
@@ -13,10 +13,10 @@ class Solution:
         for ind2 in range(size2 + 1):
             self.dp[0][ind2] = 0
 
-        for ind1 in range(0, size1):
-            for ind2 in range(0, size2):
-                ch1 = text1[ind1]
-                ch2 = text2[ind2]
+        for ind1 in range(1, size1):
+            for ind2 in range(1, size2):
+                ch1 = text1[ind1 - 1]
+                ch2 = text2[ind2 - 1]
  
                 curr_lcs_len = float('-inf')
                 if ch1 == ch2:
