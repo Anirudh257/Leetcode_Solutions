d73434d8d3531db1a46e480e193b635a6e2775c5
Anirudh257 <thatipellianirudh@gmail.com>
2025-12-31 22:17:56 -0600

    Add python solution of 128-Longest-Consecutive-Sequence.

---
+++ 128-longest-consecutive-sequence/neetcode_approach.py
@@ -0,0 +1,21 @@
+# Time complexity: O(2*n), Space complexity: O(n)
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        max_len, curr_len = 0, 0
+        num_set = set(nums)
+
+        for n in nums:
+            pre = n - 1
+            
+            # No previous number exists, so this is a potential start of sequence.
+            if pre not in num_set:
+                curr_len = 0
+
+                # keep checking if consecutive numbers exist in the set and increase
+                # the current consecutive sequence length.
+                while((n + curr_len) in num_set):
+                    curr_len += 1
+                
+                max_len = max(max_len, curr_len)
+
+        return max_len
